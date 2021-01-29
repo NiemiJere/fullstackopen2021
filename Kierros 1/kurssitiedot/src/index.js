c36d103fc904644b94673b2f902aca74261731f2
Jere Niemi <jere.niemi133@gmail.com>
2021-01-30 00:59:42 +0200

    Tehtävä 1.3

--- Kierros 1/kurssitiedot/src/index.js
+++ Kierros 1/kurssitiedot/src/index.js
@@ -22,9 +22,9 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part = {props.part1} exercises = {props.exercises1} />
-      <Part part = {props.part2} exercises = {props.exercises2} />
-      <Part part = {props.part3} exercises = {props.exercises3} />
+      <Part part = {props.part1.name} exercises = {props.part1.exercises} />
+      <Part part = {props.part2.name} exercises = {props.part2.exercises} />
+      <Part part = {props.part3.name} exercises = {props.part3.exercises} />
     </div>
   )
 }
@@ -33,7 +33,7 @@ const Total = (props) => {
   return (
     <div>
       <p>
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+      <p>Number of exercises {props.part1.exercises + props.part2.exercises + props.part3.exercises}</p>
       </p>
     </div>
   )
@@ -41,18 +41,25 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 13
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
 
   return (
     <div>
-      <Header course = {course} />
-      <Content part1 = {part1} part2 = {part2} part3 = {part3} exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3} />
-      <Total exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3} />
+        <Header course = {course} />
+        <Content part1 = {part1} part2 = {part2} part3 = {part3} />
+        <Total part1 = {part1} part2 = {part2} part3 = {part3} />
     </div>
   )
 }
